13ef9a0c526373dc5753cffaffc4ddbbc3481d69
noaa <noaamaman325158@gmail.com>
2023-12-14 10:36:18 +0200

    Define some entities like Job and Employer.

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from graphene import Schema, ObjectType, String
+from graphene import Schema, ObjectType, String, Int, List, Field
 from fastapi import FastAPI
-from starlette_graphene3 import GraphQLApp, make_graphiql_handler, make_playground_handler
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler
+
+
+class EmployerObject(ObjectType):
+    id = Int()
+    name = String()
+    contact_email = String()
+    industry = String()
+    jobs = List(lambda: JobObject)
+
+
+class JobObject(ObjectType):
+    id = Int()
+    title = String()
+    description = String()
+    employer_id = Int()
+    employer = Field(lambda: EmployerObject)
 
 
 class Query(ObjectType):
-    hello = String(name=String(default_value="graphql"))
-
-    @staticmethod
-    def resolve_hello(root, info, name):
-        return f"Hello {name}"
+    pass
 
 
 schema = Schema(query=Query)
@@ -18,9 +30,4 @@ app = FastAPI()
 app.mount("/graphql", GraphQLApp(
     schema=schema,
     on_get=make_graphiql_handler()
-))
-
-app.mount("/graphql-p", GraphQLApp(
-    schema=schema,
-    on_get=make_playground_handler()
 ))
