7284083b66bf629de8d347342f76ee7b65e123e9
noaa <noaamaman325158@gmail.com>
2023-12-14 10:18:11 +0200

    Exposing A Graphical interface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from graphene import Schema, ObjectType, String
 from fastapi import FastAPI
-from starlette_graphene3 import GraphQLApp
+from starlette_graphene3 import GraphQLApp, make_graphiql_handler, make_playground_handler
 
 
 class Query(ObjectType):
@@ -15,4 +15,12 @@ schema = Schema(query=Query)
 
 app = FastAPI()
 
-app.mount("/graphql", GraphQLApp(schema=schema))
+app.mount("/graphql", GraphQLApp(
+    schema=schema,
+    on_get=make_graphiql_handler()
+))
+
+app.mount("/graphql-p", GraphQLApp(
+    schema=schema,
+    on_get=make_playground_handler()
+))
