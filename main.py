072991a479be4fa87fb0a554db24fd43f5a369b3
noaa <noaamaman325158@gmail.com>
2023-12-14 11:28:32 +0200

    Generate Field Level Resolver - Got some problem .

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from graphene import Schema, ObjectType, String, Int, List, Field
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 
+# Static Data
+employers_data = [
+    {"id": 1, "name": "MetaTechA", "contact_email": "contact@company-a.com", "industry": "Tech"},
+    {"id": 2, "name": "MoneySoftB", "contact_email": "contact@company-b.com", "industry": "Finance"},
+]
+
+jobs_data = [
+    {"id": 1, "title": "Software Engineer", "description": "Develop web applications", "employer_id": 1},
+    {"id": 2, "title": "Data Analyst", "description": "Analyze data and create reports", "employer_id": 1},
+    {"id": 3, "title": "Accountant", "description": "Manage financial records", "employer_id": 2},
+    {"id": 4, "title": "Manager", "description": "Manage people who manage records", "employer_id": 2},
+]
+
 
 class EmployerObject(ObjectType):
     id = Int()
@@ -9,6 +22,11 @@ class EmployerObject(ObjectType):
     contact_email = String()
     industry = String()
     jobs = List(lambda: JobObject)
+
+    @staticmethod
+    def resolve_jobs(root, info):
+        # Correct the key from "employer_data" to "employer_id"
+        return [job for job in jobs_data if job["employer_id"] == root["id"]]
 
 
 class JobObject(ObjectType):
@@ -18,9 +36,23 @@ class JobObject(ObjectType):
     employer_id = Int()
     employer = Field(lambda: EmployerObject)
 
+    @staticmethod
+    def resolve_employers(root, info):
+        # Using generator expression for getting only one value
+        return next((employer for employer in employers_data if employer["id"] == root["employer_id"]), None)
+
 
 class Query(ObjectType):
-    pass
+    jobs = List(JobObject)
+    employers = List(EmployerObject)
+
+    @staticmethod
+    def resolve_jobs(root, info):
+        return jobs_data
+
+    @staticmethod
+    def resolve_employers(root, info):
+        return employers_data
 
 
 schema = Schema(query=Query)
