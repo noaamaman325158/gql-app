a2892244f07791cbe96b37c5eb44836d0c11bdf7
noaa <noaamaman325158@gmail.com>
2023-12-20 23:23:12 +0200

    Add some endpoints queries for this FastAPI.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from graphene import Schema
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
-from app.db.database import prepare_database
+from app.db.database import prepare_database, Session
 from app.gql.queries import Query
+from app.db.models import Employer, Job
 
 schema = Schema(query=Query)
 
@@ -12,6 +13,18 @@ app = FastAPI()
 @app.on_event("startup")
 def startup_event():
     prepare_database()
+
+
+@app.get('/employers')
+def get_employers():
+    with Session() as session:
+        return session.query(Employer).all()
+
+
+@app.get('/jobs')
+def get_employers():
+    with Session() as session:
+        return session.query(Job).all()
 
 
 app.mount("/graphql", GraphQLApp(
